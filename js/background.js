545f023b908b24c5e9daf66c964b07bb955da16e
Ulrich Pogson <grapplerulrich@gmail.com>
2022-09-15 15:50:56 +0200

    Initial attempt

--- js/background.js
+++ js/background.js
@@ -1,6 +1,12 @@
 (function() {
-  var PlatformExtension,
+  var PlatformCookie, PlatformExtension, reconnectAttempts, reconnectTimeout,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  PlatformCookie = this.PlatformCookie;
+
+  reconnectTimeout = null;
+
+  reconnectAttempts = 0;
 
   PlatformExtension = (function() {
     PlatformExtension.prototype.host = window.host;
@@ -11,25 +17,74 @@
 
     function PlatformExtension() {
       this.installed = bind(this.installed, this);
-      this.attach_platform_worker_listener = bind(this.attach_platform_worker_listener, this);
+      this.getTimerStatus = bind(this.getTimerStatus, this);
       this.handleHeaders = bind(this.handleHeaders, this);
       this.handleMessage = bind(this.handleMessage, this);
+      this.connect = bind(this.connect, this);
+      this.connect();
+      document.addEventListener("login:change", this.connect);
+      new PlatformCookie();
       chrome.runtime.onMessage.addListener(this.handleMessage);
       chrome.runtime.onInstalled.addListener(this.installed);
-      this.attach_platform_worker_listener();
       chrome.webRequest.onHeadersReceived.addListener(this.handleHeaders, {
         urls: ['https://github.com/*'],
         types: ['main_frame']
       }, ['blocking', 'responseHeaders']);
     }
 
+    PlatformExtension.prototype.connect = function() {
+      var ref, source, url;
+      clearTimeout(reconnectTimeout);
+      url = new URL('/platform/events', this.host);
+      url.searchParams.set('client_version', this.version);
+      if (navigator.userAgent.includes('Edg/')) {
+        url.searchParams.set('source', 'edge-extension');
+      } else if (navigator.userAgent.includes('Firefox/')) {
+        url.searchParams.set('source', 'firefox-extension');
+      } else {
+        url.searchParams.set('source', 'chrome-extension');
+      }
+      if ((ref = this.eventSource) != null) {
+        ref.close();
+      }
+      source = this.eventSource = new EventSource(url.toString());
+      this.eventSource.onopen = (function(_this) {
+        return function() {
+          return reconnectAttempts = 0;
+        };
+      })(this);
+      this.eventSource.onerror = (function(_this) {
+        return function(error) {
+          var delay;
+          if (source.readyState !== 2) {
+            return;
+          }
+          delay = 5000 * Math.pow(2, reconnectAttempts++);
+          delay += Math.random() * 10000 - 5000;
+          delay = Math.min(delay, 60 * 60 * 1000);
+          return reconnectTimeout = setTimeout(_this.connect, delay);
+        };
+      })(this);
+      return this.eventSource.onmessage = (function(_this) {
+        return function(arg) {
+          var data, ref1;
+          data = arg.data;
+          switch ((ref1 = JSON.parse(data)) != null ? ref1.type : void 0) {
+            case 'event':
+            case 'stale':
+              return _this.getTimerStatus();
+          }
+        };
+      })(this);
+    };
+
     PlatformExtension.prototype.handleMessage = function(message, sender, respond) {
       switch (message != null ? message.type : void 0) {
-        case 'harvest:browser:getHost':
+        case 'getHost':
           return respond(this.host);
-        case 'harvest:browser:timer:started':
+        case 'timer:started':
           return this.setRunningTimerIcon(true);
-        case 'harvest:browser:timer:stopped':
+        case 'timer:stopped':
           return this.setRunningTimerIcon(false);
       }
     };
@@ -52,7 +107,7 @@
         return;
       }
       return header.value = ((ref = header.value) != null ? ref : '').replace(/(child|connect|script|style|img|frame)-src/ig, function(match) {
-        return match + " " + (this.host.replace("platform", "*"));
+        return match + " " + (this.host.replace("platform", "*")) + " https://*.getharvest.com http://*.getharvest.localhost";
       });
     };
 
@@ -60,40 +115,50 @@
       return /^content-security-policy$/i.test(header.name);
     };
 
+    PlatformExtension.prototype.getTimerStatus = function() {
+      var xhr;
+      xhr = new XMLHttpRequest;
+      xhr.onload = (function(_this) {
+        return function() {
+          var ref;
+          if (xhr.status === 401) {
+            if ((ref = _this.eventSource) != null) {
+              ref.close();
+            }
+            _this.setRunningTimerIcon(false);
+          }
+          if (xhr.status === 200) {
+            return _this.setRunningTimerIcon(((function() {
+              try {
+                return JSON.parse(xhr.responseText);
+              } catch (error1) {}
+            })()) != null);
+          }
+        };
+      })(this);
+      xhr.open('get', this.host + "/platform/last_running_timer.json");
+      xhr.withCredentials = true;
+      if (navigator.userAgent.includes('Edg/')) {
+        xhr.setRequestHeader("X-HarvestEdgeExt", this.version);
+      } else if (navigator.userAgent.includes('Firefox/')) {
+        xhr.setRequestHeader("X-HarvestFirefoxExt", this.version);
+      } else {
+        xhr.setRequestHeader("X-HarvestChromeExt", this.version);
+      }
+      return xhr.send();
+    };
+
     PlatformExtension.prototype.setRunningTimerIcon = function(running) {
       var state;
       state = running ? "on" : "off";
-      chrome.browserAction.setIcon({
+      chrome.action.setIcon({
         path: {
           "19": "images/h-toolbar-" + state + "@19px.png",
           "38": "images/h-toolbar-" + state + "@38px.png"
         }
       });
-      return chrome.browserAction.setTitle({
+      return chrome.action.setTitle({
         title: running ? "View the running Harvest timer" : "Start a Harvest timer"
-      });
-    };
-
-    PlatformExtension.prototype.attach_platform_worker_listener = function() {
-      var background, worker_iframe;
-      background = this;
-      worker_iframe = "<iframe hidden src=\"" + this.host + "/platform/worker\"></iframe>";
-      document.body.insertAdjacentHTML('beforeend', worker_iframe);
-      return window.addEventListener("message", function(evt) {
-        if (!(evt.origin === this.host && evt.data)) {
-          return;
-        }
-        if (evt.data.type === "timer:started") {
-          return background.setRunningTimerIcon(true);
-        } else if (evt.data.type === "timer:stopped") {
-          return background.setRunningTimerIcon(false);
-        } else if (evt.data.type === "timer:update") {
-          if (evt.data.value) {
-            return background.setRunningTimerIcon(true);
-          } else {
-            return background.setRunningTimerIcon(false);
-          }
-        }
       });
     };
 
@@ -101,11 +166,11 @@
       var previousVersion, reason;
       reason = arg.reason, previousVersion = arg.previousVersion;
       switch (reason) {
-        case chrome.runtime.OnInstalledReason.INSTALL:
+        case 'install':
           return chrome.tabs.create({
             url: this.splashUrl + "?version=" + this.version
           });
-        case chrome.runtime.OnInstalledReason.UPDATE:
+        case 'update':
           return console.log("Upgrade notice: " + previousVersion + " upgraded to " + this.version);
       }
     };
--- js/iframe.js
+++ js/iframe.js
@@ -1,5 +1,5 @@
 (function() {
-  var canBeClosed, getService, shouldClose, waitUntilChromeAutofocuses;
+  var canBeClosed, getService, setRunningTimerIcon, shouldClose, waitUntilChromeAutofocuses;
 
   canBeClosed = true;
 
@@ -19,13 +19,10 @@
     };
   })(this));
 
-  window.addEventListener("message", function(evt) {
-    var iframe, message;
-    if (evt.origin !== this.host) {
-      return;
-    }
+  window.addEventListener("message", function(e) {
+    var iframe, isRunning, matches, message, ref;
     iframe = document.querySelector("iframe");
-    message = evt.data;
+    message = e.data;
     if (message.type === "frame:close") {
       if (canBeClosed) {
         return window.close();
@@ -34,16 +31,31 @@
       }
     } else if (message.type === "frame:resize") {
       return iframe.style.height = message.value + "px";
-    } else if (message.type === "timer:started") {
-      return chrome.runtime.sendMessage({
-        type: "harvest:browser:timer:started"
-      });
-    } else if (message.type === "timer:stopped") {
-      return chrome.runtime.sendMessage({
-        type: "harvest:browser:timer:stopped"
-      });
+    } else if (matches = (ref = message.type) != null ? ref.match(/timer:(started|stopped)/) : void 0) {
+      isRunning = matches[1] === "started";
+      return setRunningTimerIcon(isRunning);
     }
   });
+
+  setRunningTimerIcon = function(isRunning) {
+    var options, state;
+    state = isRunning ? "on" : "off";
+    canBeClosed = false;
+    options = {
+      path: {
+        "19": "images/h-toolbar-" + state + "@19px.png",
+        "38": "images/h-toolbar-" + state + "@38px.png"
+      }
+    };
+    chrome.action.setIcon(options, function() {
+      if (shouldClose) {
+        return window.close();
+      }
+    });
+    return chrome.action.setTitle({
+      title: isRunning ? "View the running Harvest timer" : "Start a Harvest timer"
+    });
+  };
 
   waitUntilChromeAutofocuses = function(element) {
     return element.getBoundingClientRect().width;
