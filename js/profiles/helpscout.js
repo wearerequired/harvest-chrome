28251cd52b637d1543a5ee8ef466f3800aa6cba5
krokodok <jeff@required.ch>
2023-12-22 01:03:14 +0100

    Add support for new Helpscout inbox
    https://github.com/wearerequired/harvest-chrome/issues/12

--- js/profiles/helpscout.js
+++ js/profiles/helpscout.js
@@ -30,7 +30,8 @@
 
     HelpscoutProfile.prototype.listen = function() {
       document.body.addEventListener("harvest-event:ready", this.addTimerIfOnIssue);
-      this.headerButton = this.createButton();
+      this.headerButtonOld = this.createButtonOld();
+      this.headerButtonNew = this.createButtonNew();
       return new MutationObserver(this.handleMutations).observe(document.body, {
         childList: true,
         subtree: true
@@ -47,7 +48,7 @@
           results1 = [];
           for (j = 0, len1 = addedNodes.length; j < len1; j++) {
             node = addedNodes[j];
-            if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('c-convo-toolbar')) {
+            if (node.nodeType === Node.ELEMENT_NODE && (node.classList.contains('c-convo-toolbar') || node.querySelector('[class*="Actionscss__ActionsUI"]'))) {
               results1.push(this.addTimerIfOnIssue());
             } else {
               results1.push(void 0);
@@ -85,29 +86,34 @@
 
     HelpscoutProfile.prototype.issueTitle = function() {
       var ref;
-      return (ref = document.querySelector('#subjectLine')) != null ? ref.innerText : void 0;
+      return (ref = document.querySelector('#subjectLine, [class*="SubjectFieldcss__Header"]')) !== null ? ref.innerText : void 0;
     };
 
     HelpscoutProfile.prototype.addTimer = function(data) {
       var actions, el, i, len, name, ref;
-      for (name in data) {
-        this.headerButton.dataset[name] = JSON.stringify(data[name]);
-      }
       ref = document.querySelectorAll('.harvest-timer');
       for (i = 0, len = ref.length; i < len; i++) {
         el = ref[i];
-        if (el !== this.headerButton) {
+        if (el !== this.headerButtonOld && el !== this.headerButtoNew) {
           el.remove();
         }
       }
-      actions = document.querySelector("ul.convo-actions");
-      if (actions != null) {
-        actions.insertBefore(this.headerButton, actions.children[0]);
+      actions = document.querySelector('ul.convo-actions, [class*="Actionscss__ActionsUI"]');
+      if (null !== actions) {
+        isOldAction = actions.classList.contains('convo-actions');
+        for (name in data) {
+          if (isOldAction) {
+            this.headerButtonOld.dataset[name] = JSON.stringify(data[name]);
+          } else {
+            this.headerButtonNew.dataset[name] = JSON.stringify(data[name]);
+          }
+        }
+        actions.insertBefore(isOldAction ? this.headerButtonOld : this.headerButtonNew, actions.children[0]);
       }
       return this.notifyPlatformOfNewTimers();
     };
 
-    HelpscoutProfile.prototype.createButton = function() {
+    HelpscoutProfile.prototype.createButtonOld = function() {
       var button, link, icon;
       button = document.createElement("li");
       button.className = "harvest-timer";
@@ -120,7 +126,19 @@
       return button;
     };
 
-    HelpscoutProfile.prototype.notifyPlatformOfNewTimers = function() {
+    HelpscoutProfile.prototype.createButtonNew = function() {
+      var button, icon;
+      button = document.createElement("a");
+      button.className = "harvest-timer harvest-timer--helpscout-new-inbox";
+      button.setAttribute("data-skip-styling", true);
+      button.style
+      icon = document.createElement("i");
+      icon.className = "icon-clock-sm";
+      button.appendChild(icon);
+      return button;
+    };
+
+    HelpscoutProfile.prototype.notifyPlatformOfNewTimers = function () {
       var evt, ref;
       evt = new CustomEvent("harvest-event:timers:chrome:add");
       return (ref = document.querySelector("#harvest-messaging")) != null ? ref.dispatchEvent(evt) : void 0;
