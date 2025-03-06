51c75a3817c24d525451284a330fe717f65cbcf2
krokodok <jeff@required.ch>
2025-03-06 08:43:58 +0100

    Add other services from upstream

---
+++ js/profiles/linear.js
@@ -0,0 +1,129 @@
+;(function () {
+  const Selectors = {
+    Main: 'main',
+    Toolbar: 'header',
+    IssueView: '[data-view-id="issue-view"]',
+  }
+
+  const platformConfig = {
+    applicationName: 'Linear',
+    permalink: 'https://linear.app/%GROUP_ID%/issue/%ITEM_ID%',
+  }
+
+  function configure() {
+    const script = document.createElement('script')
+    script.setAttribute('data-platform-config', JSON.stringify(platformConfig))
+
+    const firstScript = document.getElementsByTagName('script')[0]
+    firstScript.parentNode.insertBefore(script, firstScript)
+  }
+
+  function listen() {
+    document.body.addEventListener('harvest-event:ready', function () {
+      watchForIssueChange()
+    })
+  }
+
+  function watchForIssueChange() {
+    const config = {
+      attributes: false,
+      childList: true,
+      subtree: true,
+    }
+
+    const callback = (mutationlist, _) => {
+      for (const mutation of mutationlist) {
+        if (
+          mutation.target.matches &&
+          mutation.addedNodes.length &&
+          (mutation.target.matches(Selectors.Main) ||
+            mutation.target.matches(Selectors.Toolbar) ||
+            mutation.target.querySelector(Selectors.IssueView))
+        ) {
+          document.querySelector('.harvest-timer-container')?.remove()
+          addTimerToSidebar(mutation)
+        }
+      }
+    }
+
+    const observer = new MutationObserver(callback)
+
+    observer.observe(document.body, config)
+  }
+
+  function addTimerToSidebar(mutation) {
+    const baseURI = mutation.target.baseURI
+    const [_, groupId, __, issueId, issueSlug] = baseURI
+      .split('//')[1]
+      .split('/')
+
+    const xpath = '//main/div[2]/div/div[1]/div[2]/div/div[2]/div[last()]'
+    const addToProjectSection = document.evaluate(
+      xpath,
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    )?.singleNodeValue
+
+    if (!addToProjectSection) {
+      console.warn(
+        'No linear sidebar/contextual menu section found, not on an issue or class names/structure have changed'
+      )
+      return
+    }
+
+    const buttonContainer = buildTimerForIssue(groupId, issueId, issueSlug)
+
+    if (addToProjectSection.querySelector('.harvest-timer')) {
+      addToProjectSection.querySelector('.harvest-timer').remove()
+    }
+    addToProjectSection.append(buttonContainer)
+    notifyPlatformOfNewTimer(buttonContainer.querySelector('button'))
+  }
+
+  function buildTimerForIssue(groupId, issueId) {
+    const buttonContainer = document.createElement('div')
+    buttonContainer.classList.add('harvest-timer-container')
+
+    const header = document.createElement('div')
+    header.classList.add('harvest-timer-header')
+    header.textContent = 'Time tracking'
+    buttonContainer.appendChild(header)
+    const button = document.createElement('button')
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttribute('width', '16')
+    svg.setAttribute('height', '16')
+    svg.setAttribute('role', 'img')
+    svg.setAttribute('aria-hidden', 'true')
+    svg.setAttribute('focusable', 'false')
+    svg.setAttribute('viewBox', '0 0 14 14')
+    svg.innerHTML = `
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M7 0C10.866 0 14 3.13401 14 7C14 10.866 10.866 14 7 14C3.13401 14 0 10.866 0 7C0 3.13401 3.13401 0 7 0Z" fill="#6B6F76"/>
+      <path class="minute-hand" d="M6.25 4.25C6.25 3.83579 6.55786 2.25 7 2.25C7.44214 2.25 7.75 3.83579 7.75 4.25V5.5V7.01331C7.75 7.26331 7.5862 7.74951 6.9862 7.74951C6.57199 7.74951 6.25 7.42752 6.25 7.01331V4.25Z" fill="#F5F5F5"/>
+      <path class="hour-hand" d="M6.25 4.9995C6.25 4.58529 6.55786 2.9995 7 2.9995C7.44214 2.9995 7.75 4.58529 7.75 4.9995V6.2495V6.9995C7.75 7.2495 7.6 7.7495 7 7.7495C6.58579 7.7495 6.25 7.41371 6.25 6.9995V4.9995Z" fill="#F5F5F5"/>
+    `
+    button.appendChild(svg)
+
+    const buttonText = document.createElement('span')
+    button.appendChild(buttonText)
+
+    button.classList.add('harvest-timer')
+    button.dataset.skipStyling = true
+    button.dataset.group = JSON.stringify({ id: groupId })
+    button.dataset.item = JSON.stringify({ id: issueId, name: issueId })
+    buttonContainer.appendChild(button)
+
+    return buttonContainer
+  }
+
+  function notifyPlatformOfNewTimer(element) {
+    const detail = { element }
+    const evt = new CustomEvent('harvest-event:timers:add', { detail })
+
+    document.querySelector('#harvest-messaging').dispatchEvent(evt)
+  }
+
+  configure()
+  listen()
+}).call(this)
---
+++ js/profiles/monday.js
@@ -0,0 +1,116 @@
+;(function () {
+  const slidePanelOpenElement = '.item-board-subset-tabs-component'
+
+  const platformConfig = {
+    applicationName: 'Monday',
+    skipStyling: true,
+  }
+
+  let interval
+
+  function configure() {
+    const script = document.createElement('script')
+    script.setAttribute('data-platform-config', JSON.stringify(platformConfig))
+
+    const firstScript = document.getElementsByTagName('script')[0]
+    firstScript.parentNode.insertBefore(script, firstScript)
+  }
+
+  function listen() {
+    document.body.addEventListener('harvest-event:ready', () => {
+      watchToAddButton()
+    })
+  }
+
+  function watchToAddButton() {
+    const observer = new MutationObserver((mutationList, _) => {
+      for (const { addedNodes } of mutationList) {
+        if (!addedNodes.length) continue
+        const [addedNode] = addedNodes
+        if (!addedNode.matches) continue
+
+        if (addedNode.querySelector(slidePanelOpenElement)) {
+          whenSidePanelReadyForTimer(() => addTimer())
+        }
+      }
+    })
+
+    const config = { attributes: false, childList: true, subtree: true }
+    observer.observe(document.body, config)
+  }
+
+  function addTimer() {
+    const taskId = window.location.href.split('pulses/')[1]
+
+    if (!taskId) {
+      return
+    }
+
+    //get task name from crm/dev or work management product views
+    const viewTaskName =
+      document.querySelector('.item-page-item-name h2') ||
+      document.querySelector('.pulse_title h2')
+    //get insertion element from crm/dev or work management product views
+    const insertionEl =
+      document.querySelector('.item-page-header-component__actions-bar') ||
+      document.querySelector('.pulse_subscribers_wrapper')
+
+    const accountName = window.location.host.split('.')[0]
+    const [boardId] = window.location.pathname
+      .split('/')
+      .filter((pathValue) => /^\d+$/.test(pathValue))
+    const dataGroup = { id: parseInt(boardId, 10) }
+    const task = {
+      name: viewTaskName.innerHTML,
+      id: taskId,
+    }
+    const button = buildTimerButton(task, accountName, dataGroup)
+
+    if (insertionEl) {
+      insertionEl.insertAdjacentElement('beforebegin', button)
+      notifyPlatformOfNewTimer(button)
+    }
+  }
+
+  function whenSidePanelReadyForTimer(callback) {
+    const poll = () => {
+      if (!document.querySelector(slidePanelOpenElement)) return
+      window.clearInterval(interval)
+      callback()
+    }
+
+    window.clearInterval(interval)
+    interval = window.setInterval(poll, 200)
+  }
+
+  function buildTimerButton(task, accountName, dataGroup) {
+    const span = document.createElement('span')
+    span.classList.add('harvest-button-span')
+
+    const button = document.createElement('div')
+    button.role = 'button'
+    button.classList.add('harvest-timer')
+    button.setAttribute('data-item', JSON.stringify(task))
+    button.setAttribute('data-account', accountName)
+    button.setAttribute('data-group', JSON.stringify(dataGroup))
+    button.setAttribute('data-permalink', window.location.href)
+    button.innerHTML = `
+      <svg id="harvest-monday-svg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="8" cy="8" r="7.25" stroke="#323338" stroke-width="1.5"/>
+      <path class="minute-hand" d="M8 3V8" stroke="#323338" stroke-width="1.5" stroke-linecap="round"/>
+      <path class="hour-hand" d="M8 5V8" stroke="#323338" stroke-width="1.5" stroke-linecap="round"/>
+      </svg>
+    `
+    button.appendChild(span)
+    return button
+  }
+
+  function notifyPlatformOfNewTimer(element) {
+    const detail = { element }
+    const evt = new CustomEvent('harvest-event:timers:chrome:add', { detail })
+    document.querySelector('#harvest-messaging').dispatchEvent(evt)
+  }
+
+  configure()
+  listen()
+}).call(this)
